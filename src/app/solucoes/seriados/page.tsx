367844760bae7484d21e1553ebc0301d8cf4773e
matheusbarcellosdev <matheusbarcellos61@gmail.com>
2023-07-27 09:02:46 -0300

    feat: Add pages de redirect

---
+++ src/app/solucoes/seriados/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useEffect } from 'react'
+import { redirect, usePathname } from 'next/navigation'
+
+import Mocks from '@/mocks/linhas.json'
+import slugify from '@/utils'
+
+
+export default function SeriadosPageRedirect() {
+    const pathname = usePathname()
+
+    const pathElements = pathname.split('/').filter((element) => element.trim() !== '')
+
+    useEffect(() => {
+      const matchingSolution = Mocks.solutions.find((solution) => solution.slug === pathElements[1] )
+  
+      if (matchingSolution?.linhas != null && matchingSolution.linhas.length !== 0) {
+        const firstCategory = Object.keys(matchingSolution.linhas[0])[0]
+        const firstCategorySlug = slugify(firstCategory)
+        redirect(`/solucoes/seriados/${firstCategorySlug}`)
+      } else {
+        redirect('/')
+      }
+    }, [])
+  
+    return null
+  }
+
+
