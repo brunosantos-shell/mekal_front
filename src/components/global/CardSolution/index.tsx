2aed88ceee9832c6dd91c7eed3c1e28f0613225c
matheusbarcellosdev <matheusbarcellos61@gmail.com>
2023-08-06 20:14:51 -0300

    feat: Component CardSolution

--- src/components/global/CardSolution/index.tsx
+++ src/components/global/CardSolution/index.tsx
@@ -1,50 +1,81 @@
 import Image from 'next/image'
 import styles from './styles.module.scss'
 import { IconPlusButton } from '@/icons'
+import Link from 'next/link'
 
 interface CardSolutionProps {
   title: string;
   image: string;
   alt?: string;
-  type: 'seriados' | 'modulados' | 'sob-medida' | 'corporativo' | 'art-e-design';
+  type?: 'seriados' | 'modulados' | 'sob-medida' | 'corporativo' | 'art-e-design';
+  numeric?: string;
+  width?: number;
+  height?: number;
+  tablet?: boolean;
+  responsive?: boolean;
+  url?: string;
 }
 
-export default function CardSolution({ title, image, alt, type }: CardSolutionProps) {
-  let width, height
+export default function CardSolution({
+  title,
+  image,
+  alt,
+  type,
+  numeric,
+  width,
+  height,
+  tablet,
+  responsive,
+  url
+}: CardSolutionProps) {
+  let defaultWidth, defaultHeight
+
   switch (type) {
     case 'seriados':
-      width = 384
-      height = 384
+      defaultWidth = 384
+      defaultHeight = 384
       break
     case 'modulados':
-      width = 384
-      height = 600
+      defaultWidth = 384
+      defaultHeight = 600
       break
     case 'sob-medida':
     case 'corporativo':
-      width = 384
-      height = 384
+      defaultWidth = 384
+      defaultHeight = 384
       break
     case 'art-e-design':
-      width = 616
-      height = 384
+      defaultWidth = 616
+      defaultHeight = 384
       break
     default:
-      width = 384
-      height = 384
+      defaultWidth = 384
+      defaultHeight = 384
   }
 
+  const finalWidth = width !== undefined ? width : defaultWidth
+  const finalHeight = height !== undefined ? height : defaultHeight
+
   return (
-    <div className={styles.card_solution}
-        style={{ maxWidth: `${width}px`, maxHeight: `${height}px` }}
-    >
-      <div className={styles.image_wrapper__card_solution}>
-        <Image src={image} alt={title} width={width} height={height} />
-        <button className={styles.card_product__btn__view_solution}>
-        <IconPlusButton />
-      </button>
+
+      <div className={`${styles.container_card_solution} ${(responsive ?? false) ? 'responsive' : ''}`}>
+        <Link
+        href={url ?? '#'}
+      >
+      <div className={styles.card_solution__numeric}>
+        <span>{numeric}</span>
       </div>
-      <h4>{title}</h4>
+        <div className={`${styles.card_solution} ${styles[type ?? '']} ${(responsive ?? false) ? styles.responsive : ''}`} style={{ maxWidth: `${finalWidth}px`, maxHeight: `${finalHeight}px` }}>
+        <div className={styles.image_wrapper__card_solution}>
+          <Image src={image} alt={title} width={finalWidth} height={finalHeight} quality={100}/>
+          <button className={styles.card_product__btn__view_solution}>
+            <IconPlusButton />
+          </button>
+        </div>
+      </div>
+      <h4 className={styles.card_solution__title}>{title}</h4>
+      </Link>
     </div>
+   
   )
 }
